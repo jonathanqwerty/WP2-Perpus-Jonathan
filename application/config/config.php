3d56e687686ca1963e60082e5485fbfd2cea0a07
jonathanqwerty <nathanaeljonathan09@gmail.com>
2023-05-11 15:39:40 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/perpustakaan/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
